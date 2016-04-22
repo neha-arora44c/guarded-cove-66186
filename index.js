501d67dd243d416c32291aab2f23ab00ae00c48c
System Administrator <root@Nehas-MacBook-Air-2.local>
2016-04-22 23:28:57 +0530

    changed index.js

--- index.js
+++ index.js
@@ -23,22 +23,3 @@ app.get('/webhook/', function (req, res) {
     }
     res.send('Error, wrong token')
 })
-
-// Spin up the server
-app.listen(app.get('port'), function() {
-    console.log('running on port', app.get('port'))
-})
-
-app.post('/webhook/', function (req, res) {
-    messaging_events = req.body.entry[0].messaging
-    for (i = 0; i < messaging_events.length; i++) {
-        event = req.body.entry[0].messaging[i]
-        sender = event.sender.id
-        if (event.message && event.message.text) {
-            text = event.message.text
-            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-        }
-    }
-    res.sendStatus(200)
-})
-
